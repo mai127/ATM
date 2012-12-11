8c257cbdb8218e23ce817ffecaa50256b4962673
kleanth <mai1210@uom.gr>
2012-12-11 19:39:49 +0200

    commit transaction withdraw

---
+++ ATM/src/Withdraw.java
@@ -0,0 +1,20 @@
+
+public class Withdraw extends Transaction{
+	private double amount;
+	private Account account;
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	
+	public void Withdrawl(){
+		int ammount=0;
+		if(acount.getBalance>=ammount)
+			account.getBalance-=ammount;
+		
+	}
+}
