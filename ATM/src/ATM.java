4f361c9e8f2e965bf5a58279d4391bc7c9cbb9db
Chris K. <mai127@uom.gr>
2012-12-11 21:27:07 +0200

    jhjk

--- ATM/src/ATM.java
+++ ATM/src/ATM.java
@@ -3,7 +3,7 @@ public class ATM {
 	
 	public static void main(String args[])
     {
-       System.out.println("test ATM project!");
+       System.out.println("test ATM project///!");
        
        System.out.println("Another push testing!");
        
