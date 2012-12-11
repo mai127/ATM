62d5a17ffa6b5ada0f090b22ecef31b524745ac8
Chris K. <mai127@uom.edu.gr>
2012-12-11 15:33:51 +0200

    Another Push testing!

--- ATM/src/ATM.java
+++ ATM/src/ATM.java
@@ -4,5 +4,7 @@ public class ATM {
 	public static void main(String args[])
     {
        System.out.println("test ATM project!");
+       
+       System.out.println("Another push testing!");
     }
 }
