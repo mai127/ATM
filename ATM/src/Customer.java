e099c46326c8503f6bbefc3d2eb20be697c050bd
Chris K. <mai127@uom.edu.gr>
2013-01-07 18:41:46 +0200

    methods for class Customer

--- ATM/src/Customer.java
+++ ATM/src/Customer.java
@@ -1,6 +1,22 @@
 
 public class Customer {
+	
 	private String name;
 	private String address;
-	private String dob;
+	
+	public String getName() {
+		return name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	private Bank bank;
+	private Account account;
 }
