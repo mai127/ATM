d24ae358da52e61243f31cb0d93911e7666c4fac
Chris K. <mai127@uom.edu.gr>
2013-01-07 18:32:03 +0200

    Methods for 'CurrentAccount' and 'SavingsAccount' classes

--- ATM/src/CurrentAccount.java
+++ ATM/src/CurrentAccount.java
@@ -1,4 +1,20 @@
 
 public class CurrentAccount extends Account {
-
+	
+	private double AccountNumber;
+	private double balance;
+	
+	public double getAccountNumber() {
+		return AccountNumber;
+	}
+	public void setAccountNumber(double AccountNumber) {
+		this.AccountNumber = AccountNumber;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
 }
--- ATM/src/SavingsAccount.java
+++ ATM/src/SavingsAccount.java
@@ -1,4 +1,19 @@
 
 public class SavingsAccount extends Account {
 
+	private double AccountNumber;
+	private double balance;
+	
+	public double getAccountNumber() {
+		return AccountNumber;
+	}
+	public void setAccountNumber(double AccountNumber) {
+		this.AccountNumber = AccountNumber;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 }
