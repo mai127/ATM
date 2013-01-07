7811995027d0ad85dd3527e597b01e5619853e32
Chris K. <mai127@uom.edu.gr>
2013-01-07 18:46:43 +0200

    methods for 'Account' class

--- ATM/src/Account.java
+++ ATM/src/Account.java
@@ -4,4 +4,16 @@ public abstract class Account {
 	private String type;
 	private String owner;
 
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
 }
