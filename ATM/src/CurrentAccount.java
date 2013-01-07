98403ba8880398b0f1c50e53671a75c012b62ad4
Chris K. <mai127@uom.edu.gr>
2013-01-07 19:54:23 +0200

    credit and debit methods for 'CurrentAccount' and 'SavingsAccount'classes

--- ATM/src/CurrentAccount.java
+++ ATM/src/CurrentAccount.java
@@ -1,9 +1,11 @@
 
 public class CurrentAccount extends Account {
-	
+	 
+	  // attributes
 	private double AccountNumber;
 	private double balance;
 	
+	  // operations
 	public double getAccountNumber() {
 		return AccountNumber;
 	}
@@ -16,5 +18,11 @@ public class CurrentAccount extends Account {
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
-	
+		// σε περίπτωση που κρατήσουμε το type
+	public void credit(int amount) {  
+		balance += amount;
+	}
+	public void debit(int amount) {
+		balance += amount;
+	}
 }
--- ATM/src/SavingsAccount.java
+++ ATM/src/SavingsAccount.java
@@ -1,6 +1,7 @@
 
 public class SavingsAccount extends Account {
 
+	
 	private double AccountNumber;
 	private double balance;
 	
@@ -15,5 +16,12 @@ public class SavingsAccount extends Account {
 	}
 	public void setBalance(double balance) {
 		this.balance = balance;
+	}
+		// σε περίπτωση που κρατήσουμε το type
+	public void credit(int amount) {  
+		balance += amount;
+	}
+	public void debit(int amount) {
+		balance += amount;
 	}
 }
