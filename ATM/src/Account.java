ae0133310c52817cf729a434e16572dc738003f5
Octapus <Octapus@Octapus-PC>
2013-01-08 17:55:10 +0200

    Improved 'Account' class

--- ATM/src/Account.java
+++ ATM/src/Account.java
@@ -1,19 +1,33 @@
 
 public abstract class Account {
 
-	private String type;
 	private String owner;
+	private int AccountNumber;
+	private double balance;
 
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
 	public String getOwner() {
 		return owner;
 	}
 	public void setOwner(String owner) {
 		this.owner = owner;
+	}
+	public double getAccountNumber() {
+		return AccountNumber;
+	}
+	public void setAccountNumber(int AccountNumber) {
+		this.AccountNumber = AccountNumber;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	public void debit() {
+		
+	}
+	public void credit() {
+		
 	}
 }
