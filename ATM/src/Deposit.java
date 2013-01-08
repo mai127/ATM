7387733a8d5c196d158b4b48900be00170e6dc08
Octapus <Octapus@Octapus-PC>
2013-01-08 17:44:27 +0200

    Create new class 'deposit'

---
+++ ATM/src/Deposit.java
@@ -0,0 +1,15 @@
+
+public class Deposit extends Transaction {
+		
+	private double amount;
+	private Account account;
+	
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+		
+}
