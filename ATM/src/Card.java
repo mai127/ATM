c7768cd2ca25ceda22f9a4c852f7e4bed3d095d7
kleanthis <kleanthis@Netbook>
2013-01-08 18:12:13 +0200

    card access method

--- ATM/src/Card.java
+++ ATM/src/Card.java
@@ -2,4 +2,23 @@
 public abstract class Card {
 	private String cardNO;
 	private String ownedBY;
+	private String pin;
+	
+	public void setPin (String p){
+		
+		pin=p;
+	}
+	
+	public String getPin(){
+		
+		return pin;
+	}
+	
+	public boolean access (String p){
+		if(pin==p)
+			return true;
+		else
+			return false;
+		
+	}
 }
