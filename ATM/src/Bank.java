30ce9497d37c6c09782d3303f6aa563269df4e2c
kleanth <mai1210@uom.gr>
2013-01-10 19:20:15 +0200

    new commits

--- ATM/src/Bank.java
+++ ATM/src/Bank.java
@@ -1,4 +1,3 @@
-import java.sql.*;
 
 public class Bank {
 	
@@ -7,7 +6,7 @@ public class Bank {
 		Card crd = new Card(1234567890,"UOM",1234);
 		Account accnt = new Account("Papadopoulos Giannis", 1234554321, 1020.09, crd);
 		Customer custmr = new Customer("Papadopoulos Giannis", "Egnatia 127 str.", accnt);
-		PinValidation pinvld=new PinValidation(12343,accnt);
+		PinValidation pinvld=new PinValidation(1234,accnt);
 		
 		
 		ATMInfo atmInf = new ATMInfo("Aristoteloys sqr. 165", "Beta", pinvld);
@@ -20,38 +19,7 @@ public class Bank {
 		atmInf.identifies();
 	 
 				
-		try {
-			  // Load the JDBC driver 
-			Class.forName("org.sqlite.JDBC");
-			  // Establish the connection to the database ATM
-			String db = "jdbc:sqlite:ATM.db";
-			  // create database connection
-			Connection con = DriverManager.getConnection(db);
-			  // Creates some Statement objects for sending SQL statements to the database
-			Statement st1 = con.createStatement();
-			
-			String sql = ("create table if not exists 'Customer' (Name String, Address String)");
-			st1.executeUpdate(sql);
-				
-			st1.executeUpdate("insert into Customer values ('Gigi K.', 'Egnatia str. 12')");
-			st1.executeUpdate("insert into Customer values ('Kiki G.', 'Tsimiski str. 13')");
-			  // query execution
-			String query = "select * from Customer";
-			ResultSet rs = st1.executeQuery(query);
-			 
-			// prints query results
-			while (rs.next()) {
-				System.out.print(rs.getString(1)+"\t\t");
-				System.out.println(rs.getString(2));
-			}	
-			
-			rs.close();
-			st1.close();
-			con.close();
-		} // end of try 
-		catch(Exception e) {
-			System.out.println(e);
-		} // end of catch
+		
 		
 	} // end of main 
 } // end of Bank class
