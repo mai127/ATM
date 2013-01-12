03da6ab5850ff3b18713048177657742cf7cc361
Chris K. <mai127@uom.edu.gr>
2013-01-12 19:09:04 +0200

    gui with some functionality ([0-9] & clear works)

--- ATM/src/ATM_gui1.java
+++ ATM/src/ATM_gui1.java
@@ -8,6 +8,8 @@ import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class ATM_gui1 {
@@ -48,43 +50,89 @@ public class ATM_gui1 {
 		frame.getContentPane().setLayout(null);
 		
 		textField = new JTextField();
+		textField.setEditable(false);
 		textField.setBounds(109, 26, 335, 134);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JButton btnNewButton = new JButton("1");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textField.setText(textField.getText()+"1");
+			}
+		});
 		btnNewButton.setBounds(133, 183, 45, 23);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton button = new JButton("2");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+"2");
+			}
+		});
 		button.setBounds(183, 183, 45, 23);
 		frame.getContentPane().add(button);
 		
 		JButton button_1 = new JButton("3");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+"3");
+			}
+		});
 		button_1.setBounds(233, 183, 45, 23);
 		frame.getContentPane().add(button_1);
 		
 		JButton button_2 = new JButton("4");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+"4");
+			}
+		});
 		button_2.setBounds(133, 217, 45, 23);
 		frame.getContentPane().add(button_2);
 		
 		JButton button_3 = new JButton("5");
+		button_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+"5");
+			}
+		});
 		button_3.setBounds(183, 217, 45, 23);
 		frame.getContentPane().add(button_3);
 		
 		JButton button_4 = new JButton("6");
+		button_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+"6");
+			}
+		});
 		button_4.setBounds(233, 217, 45, 23);
 		frame.getContentPane().add(button_4);
 		
 		JButton button_5 = new JButton("9");
+		button_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+"9");
+			}
+		});
 		button_5.setBounds(233, 251, 45, 23);
 		frame.getContentPane().add(button_5);
 		
 		JButton button_6 = new JButton("8");
+		button_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+"8");
+			}
+		});
 		button_6.setBounds(183, 251, 45, 23);
 		frame.getContentPane().add(button_6);
 		
 		JButton button_7 = new JButton("7");
+		button_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+"7");
+			}
+		});
 		button_7.setBounds(133, 251, 45, 23);
 		frame.getContentPane().add(button_7);
 		
@@ -93,6 +141,11 @@ public class ATM_gui1 {
 		frame.getContentPane().add(button_8);
 		
 		JButton button_9 = new JButton("0");
+		button_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(textField.getText()+"0");
+			}
+		});
 		button_9.setBounds(183, 287, 45, 23);
 		frame.getContentPane().add(button_9);
 		
@@ -105,6 +158,11 @@ public class ATM_gui1 {
 		frame.getContentPane().add(btnNewButton_1);
 		
 		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText("");
+			}
+		});
 		btnClear.setBounds(306, 217, 89, 23);
 		frame.getContentPane().add(btnClear);
 		
